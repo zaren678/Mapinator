4baabfc6c8e86d913c42090ed4284e810fc59843
John Anderson <zaren678@gmail.com>
2015-08-22 17:25:28 -0700

    fix license, tabbing in build script

--- xplanet/build_xplanet.sh
+++ xplanet/build_xplanet.sh
@@ -6,80 +6,80 @@ set -o pipefail
 
 function build_zlib
 {
-echo ""
-echo "************************************************"
-echo "Building zlib into folder $PREFIX"
-cd ./src/zlib-1.2.8/
-./configure \
-    --prefix=$PREFIX
-make clean
-make -j3
-make install
-cd ../..
+  echo ""
+  echo "************************************************"
+  echo "Building zlib into folder $PREFIX"
+  cd ./src/zlib-1.2.8/
+  ./configure \
+      --prefix=$PREFIX
+  make clean
+  make -j3
+  make install
+  cd ../..
 }
 
 function build_libpng
 {
-echo ""
-echo "************************************************"
-echo "Building libppng into folder $PREFIX"
-cd ./src/libpng-1.6.18/
-./configure \
-    --prefix=$PREFIX \
-    --with-zlib-prefix=$PREFIX
-make clean
-make -j3
-make install
-cd ../..
+  echo ""
+  echo "************************************************"
+  echo "Building libppng into folder $PREFIX"
+  cd ./src/libpng-1.6.18/
+  ./configure \
+      --prefix=$PREFIX \
+      --with-zlib-prefix=$PREFIX
+  make clean
+  make -j3
+  make install
+  cd ../..
 }
 
 function build_libjpeg
 {
-echo ""
-echo "************************************************"
-echo "Building libjpeg into folder $PREFIX"
-cd ./src/jpeg-9a/
-./configure \
-    --prefix=$PREFIX
-make clean
-make -j3
-make install
-cd ../..
+  echo ""
+  echo "************************************************"
+  echo "Building libjpeg into folder $PREFIX"
+  cd ./src/jpeg-9a/
+  ./configure \
+      --prefix=$PREFIX
+  make clean
+  make -j3
+  make install
+  cd ../..
 }
 
 function build_libfreetype
 {
-echo ""
-echo "************************************************"
-echo "Building libfreetype into folder $PREFIX"
-cd ./src/freetype-2.6/
-./configure \
-    --prefix=$PREFIX
-make clean
-make -j3
-make install
-cd ../..
+  echo ""
+  echo "************************************************"
+  echo "Building libfreetype into folder $PREFIX"
+  cd ./src/freetype-2.6/
+  ./configure \
+      --prefix=$PREFIX
+  make clean
+  make -j3
+  make install
+  cd ../..
 }
 
 function build_xplanet
 {
-echo ""
-echo "************************************************"
-echo "Building xPlanet into folder $PREFIX"
+  echo ""
+  echo "************************************************"
+  echo "Building xPlanet into folder $PREFIX"
 
-export FREETYPE_CONFIG=$PREFIX/bin/freetype-config
+  export FREETYPE_CONFIG=$PREFIX/bin/freetype-config
 
-cd ./src/xplanet-1.3.0/
-./configure \
-    --prefix=$PREFIX \
-    --with-png\
-    --with-jpeg\
-    --with-freetype\
-    $ADDITIONAL_CONFIGURE_FLAG
-make clean
-make -j3
-make install
-cd ../..
+  cd ./src/xplanet-1.3.0/
+  ./configure \
+      --prefix=$PREFIX \
+      --with-png\
+      --with-jpeg\
+      --with-freetype\
+      $ADDITIONAL_CONFIGURE_FLAG
+  make clean
+  make -j3
+  make install
+  cd ../..
 }
 
 PREFIX=$(pwd)/build
